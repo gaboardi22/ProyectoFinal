898e603e620e4ab691526b3c3506319d918d739e
gaboardi22 <andresgaboardi@gmail.com>
2024-10-22 18:52:57 -0300

    .
    sfhg

--- src/EntidadesData/PacienteData.java
+++ src/EntidadesData/PacienteData.java
@@ -41,4 +41,19 @@ public class PacienteData {
         }
     }
     
+    public void modificarPaciente(Paciente paciente){
+        String sql = "UPDATE paciente SET nombre = ?, edad = ?, altura = ?, peso_actual = ?, peso_esperado  = ? WHERE id_paciente = ? ";
+        
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, paciente.getNombre());
+            ps.setInt(2, paciente.getEdad());
+            ps.setDouble(3, paciente.getAltura());
+            ps.setDouble(4, paciente.getPesoActual());
+            ps.setDouble(5, paciente.getPesoEsperado());
+        } catch (SQLException ex) {
+           JOptionPane.showMessageDialog(null, "error al acceder a la tabla nutricionista");
+        }
+    }
+    
 }

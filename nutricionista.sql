df830ccb912ebf661a916d0099f4c2551b77a21f
Ezequiel <130161620+cachissj3@users.noreply.github.com>
2024-10-24 19:22:31 -0300

    .
    nuevas tablas y nuevas clases.

--- nutricionista.sql
+++ nutricionista.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-10-2024 a las 19:23:50
+-- Tiempo de generación: 25-10-2024 a las 00:21:37
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -35,7 +35,8 @@ CREATE TABLE `comida` (
   `tipo_comida` enum('DESAYUNO','MERIENDA','COLACION','ALMUERZO','CENA') NOT NULL,
   `calorias_x_100g` int(11) NOT NULL,
   `detalle` varchar(30) NOT NULL,
-  `baja` tinyint(1) NOT NULL
+  `baja` tinyint(1) NOT NULL,
+  `idIngrediente` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -52,6 +53,18 @@ CREATE TABLE `dieta` (
   `estado` tinyint(1) DEFAULT NULL,
   `totalCalorias` int(11) DEFAULT NULL,
   `idPaciente` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ingrediente`
+--
+
+CREATE TABLE `ingrediente` (
+  `idIngrediente` int(11) NOT NULL,
+  `nombre` varchar(30) DEFAULT NULL,
+  `calorias` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -97,6 +110,22 @@ CREATE TABLE `reglondemenu` (
   `subTotalCalorias` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reporte`
+--
+
+CREATE TABLE `reporte` (
+  `idReporte` int(11) NOT NULL,
+  `pesoInicial` double DEFAULT NULL,
+  `pesoFinal` double DEFAULT NULL,
+  `pesoBuscado` double DEFAULT NULL,
+  `caloriasConsumidas` double DEFAULT NULL,
+  `observacion` varchar(500) DEFAULT NULL,
+  `idDieta` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Índices para tablas volcadas
 --
@@ -105,7 +134,8 @@ CREATE TABLE `reglondemenu` (
 -- Indices de la tabla `comida`
 --
 ALTER TABLE `comida`
-  ADD PRIMARY KEY (`id_comida`);
+  ADD PRIMARY KEY (`id_comida`),
+  ADD KEY `fk_idIngrediente` (`idIngrediente`);
 
 --
 -- Indices de la tabla `dieta`
@@ -113,6 +143,12 @@ ALTER TABLE `comida`
 ALTER TABLE `dieta`
   ADD PRIMARY KEY (`idDieta`),
   ADD KEY `idPaciente` (`idPaciente`);
+
+--
+-- Indices de la tabla `ingrediente`
+--
+ALTER TABLE `ingrediente`
+  ADD PRIMARY KEY (`idIngrediente`);
 
 --
 -- Indices de la tabla `menu`
@@ -136,6 +172,13 @@ ALTER TABLE `reglondemenu`
   ADD KEY `idComida` (`idComida`);
 
 --
+-- Indices de la tabla `reporte`
+--
+ALTER TABLE `reporte`
+  ADD PRIMARY KEY (`idReporte`),
+  ADD KEY `idDieta` (`idDieta`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -148,6 +191,12 @@ ALTER TABLE `comida`
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `comida`
+--
+ALTER TABLE `comida`
+  ADD CONSTRAINT `fk_idIngrediente` FOREIGN KEY (`idIngrediente`) REFERENCES `ingrediente` (`idIngrediente`);
 
 --
 -- Filtros para la tabla `dieta`
@@ -167,6 +216,12 @@ ALTER TABLE `menu`
 --
 ALTER TABLE `reglondemenu`
   ADD CONSTRAINT `reglondemenu_ibfk_1` FOREIGN KEY (`idComida`) REFERENCES `comida` (`id_comida`);
+
+--
+-- Filtros para la tabla `reporte`
+--
+ALTER TABLE `reporte`
+  ADD CONSTRAINT `reporte_ibfk_1` FOREIGN KEY (`idDieta`) REFERENCES `dieta` (`idDieta`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

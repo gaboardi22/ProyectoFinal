0971fcb5aa71a671e66e6e0a4b14c67d6c92f71f
Ezequiel <130161620+cachissj3@users.noreply.github.com>
2024-11-13 23:40:22 -0300

    .
    validacion para que las dietas no tengan mas de 7 menus diarios

--- src/Persistencia/MenuDiarioData.java
+++ src/Persistencia/MenuDiarioData.java
@@ -26,22 +26,26 @@ public class MenuDiarioData {
     }
 
     public void guardarMenuDiario(MenuDiario menu) {
-        String sql = "INSERT INTO menus_diarios (dia, calorias_totales, id_dieta) "
-                + "VALUES (?, ?, ?)";
-        try {
-            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            ps.setInt(1, menu.getDia());
-            ps.setInt(2, menu.getCalorias());
-            ps.setInt(3, menu.getDieta().getId_dieta());
-            ps.executeUpdate();
-            ResultSet rs = ps.getGeneratedKeys();
-            if (rs.next()) {
-                JOptionPane.showMessageDialog(null, "Menu diario guardado");
-                menu.setId_menu_diario(rs.getInt(1));
+        if (contarMenusDiarios(menu.getDieta().getId_dieta()) < 7) {
+            String sql = "INSERT INTO menus_diarios (dia, calorias_totales, id_dieta) "
+                    + "VALUES (?, ?, ?)";
+            try {
+                PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                ps.setInt(1, menu.getDia());
+                ps.setInt(2, menu.getCalorias());
+                ps.setInt(3, menu.getDieta().getId_dieta());
+                ps.executeUpdate();
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    JOptionPane.showMessageDialog(null, "Menu diario guardado");
+                    menu.setId_menu_diario(rs.getInt(1));
+                }
+                ps.close();
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error al acceder a la tabla 'menu_diario'");
             }
-            ps.close();
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla 'menu_diario'");
+        } else {
+            JOptionPane.showMessageDialog(null, "Límite de menús diarios alcanzado (7)");
         }
 
     }
@@ -70,7 +74,7 @@ public class MenuDiarioData {
         try {
             PreparedStatement ps = con.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 MenuDiario menu = new MenuDiario();
                 menu.setId_menu_diario(rs.getInt("id_menu_diario"));
                 menu.setDia(rs.getInt("dia"));
@@ -86,15 +90,16 @@ public class MenuDiarioData {
 
         return lista;
     }
-    public MenuDiario buscarMenuPorID(int id){
+
+    public MenuDiario buscarMenuPorID(int id) {
         MenuDiario menu = null;
         DietaData dd = new DietaData();
-            String sql = "SELECT * FROM menus_diarios WHERE id_menu_diario = ?";
+        String sql = "SELECT * FROM menus_diarios WHERE id_menu_diario = ?";
         try {
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setInt(1, id);
             ResultSet rs = ps.executeQuery();
-            if(rs.next()){
+            if (rs.next()) {
                 menu = new MenuDiario();
                 menu.setId_menu_diario(rs.getInt("id_menu_diario"));
                 menu.setDia(rs.getInt("dia"));
@@ -106,5 +111,22 @@ public class MenuDiarioData {
             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla 'menu_diario'");
         }
         return menu;
+    }
+
+    public int contarMenusDiarios(int idDieta) {
+        String sql = "SELECT COUNT(*) FROM menus_diarios WHERE id_dieta = ?";
+        int num = 0;
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, idDieta);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                num = rs.getInt(1);
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla 'menu_diario'");
+        }
+        return num;
     }
 }
